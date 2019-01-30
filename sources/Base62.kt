f6a1e23c6366aaa53f0955ca0778d5cabaf1eb18
Marc Knaup <marc@knaup.io>
2019-01-30 13:39:29 +0100

    added Base62

---
+++ sources/Base62.kt
@@ -0,0 +1,28 @@
+package com.github.fluidsonic.fluid.stdlib
+
+import java.security.SecureRandom
+
+
+object Base62 {
+
+	private val characters = charArrayOf('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
+	private val random = SecureRandom()
+
+
+	fun random(length: Int): String {
+		check(length >= 0) { "'length' must not be negative" }
+
+		if (length == 0)
+			return ""
+
+		return buildString {
+			repeat(length) {
+				append(randomCharacter())
+			}
+		}
+	}
+
+
+	fun randomCharacter() =
+		characters[random.nextInt(characters.size)]
+}
