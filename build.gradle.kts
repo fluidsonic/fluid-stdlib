7c35ee402943dccaf1faa25dcbd4f14c1b9f5c7a
Marc Knaup <marc@knaup.io>
2019-06-05 11:44:01 +0100

    fluid-stdlib 0.9.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ kotlin {
 	sourceSets {
 		commonMain {
 			dependencies {
-				api(fluid("time", "0.9.7"))
+				api(fluid("time", "0.9.8"))
 
 				implementation(kotlinx("serialization-runtime", "0.11.0"))
 			}
