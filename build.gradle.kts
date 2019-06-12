e4bf9cfea3a8fa82ecbbf10648a2bd34f18dbbc1
Marc Knaup <marc@knaup.io>
2019-06-12 03:14:54 +0100

    fluid-time 0.9.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ kotlin {
 	sourceSets {
 		commonMain {
 			dependencies {
-				api(fluid("time", "0.9.9"))
+				api(fluid("time", "0.9.10"))
 
 				implementation(kotlinx("serialization-runtime", "0.11.0"))
 			}
