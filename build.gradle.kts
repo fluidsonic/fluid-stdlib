367a7883dc1265f021e7809796bb2b7c670a4ed4
Marc Knaup <marc@knaup.io>
2019-10-19 17:18:53 +0800

    Updated fluid-gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import io.fluidsonic.gradle.*
 
 plugins {
-	id("io.fluidsonic.gradle") version "1.0.0"
+	id("io.fluidsonic.gradle") version "1.0.1"
 }
 
 fluidLibrary(name = "stdlib", version = "0.9.28")
