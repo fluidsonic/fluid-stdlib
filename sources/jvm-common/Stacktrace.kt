462510c72d3b304f4845a40769b3844bb0fff247
Marc Knaup <marc@knaup.io>
2020-05-11 03:28:20 +0700

    Added stackTrace()

---
+++ sources/jvm-common/Stacktrace.kt
@@ -0,0 +1,5 @@
+package io.fluidsonic.stdlib
+
+
+fun stackTrace(skipCount: Int = 0) =
+	Thread.currentThread().stackTrace.drop(skipCount + 2) // +1 to exclude this, +1 to exclude Thread.getStackTrace()
