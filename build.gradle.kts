c9db2b332556fb98880eb5f3ac25ea322ed457d7
Marc Knaup <marc@knaup.io>
2020-07-21 18:13:45 +0100

    Updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import io.fluidsonic.gradle.*
 
 plugins {
-	id("io.fluidsonic.gradle") version "1.0.12"
+	id("io.fluidsonic.gradle") version "1.0.13"
 }
 
 fluidLibrary(name = "stdlib", version = "0.9.31")
