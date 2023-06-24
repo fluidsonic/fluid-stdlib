b62fd898d1f2600fd5ffba1a540c5edf61b80e47
Marc Knaup <marc@knaup.io>
2023-06-24 08:47:29 +0700

    🎉 0.13.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,16 @@
 import io.fluidsonic.gradle.*
 
 plugins {
-	id("io.fluidsonic.gradle") version "1.2.1"
+	id("io.fluidsonic.gradle") version "1.3.1"
 }
 
-fluidLibrary(name = "stdlib", version = "0.12.0")
+fluidLibrary(name = "stdlib", version = "0.13.0")
 
 fluidLibraryModule(description = "Potentially useful Kotlin standard library additions") {
 	targets {
 		common {
 			dependencies {
-				implementation(fluid("locale", "0.12.0"))
+				implementation(fluid("locale", "0.13.0"))
 			}
 		}
 		darwin()
